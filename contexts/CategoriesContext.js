cf9fa07089058ef78befb0ca14d79bb027d4a464
Julian Alfonso Ponce <ponce.julianalfonso@gmail.com>
2021-07-11 19:49:08 +1000

    Added categoriesMap to CategoriesContext

--- contexts/CategoriesContext.js
+++ contexts/CategoriesContext.js
@@ -7,6 +7,7 @@ export const CategoriesContext = React.createContext({
 
 export const CategoriesContainer = ({ children }) => {
   const [categories, setCategories] = useState([]);
+  const [categoriesMap, setCategoriesMap] = useState({});
 
   useEffect(() => {
     (async() => {
@@ -14,6 +15,30 @@ export const CategoriesContainer = ({ children }) => {
       setCategories(apiCategories);
     })();
   }, []);
+
+  useEffect(() => {
+    if (categories.length === 0) {
+      return;
+    }
+
+    const cm = {};
+
+    categories.forEach((category) => {
+      if (category.parent_id === null) {
+        cm[category.id] = {
+          name: category.name,
+          subCategories: [],
+        }
+      } else {
+        cm[category.parent_id].subCategories.push({
+          id: category.id,
+          name: category.name,
+        });
+      }
+    });
+
+    setCategoriesMap(cm);
+  }, [categories]);
 
   const contextValue = {
     categories,
